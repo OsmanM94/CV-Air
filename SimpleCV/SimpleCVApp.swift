403ff0db40f4150bcfe2861df4348243e9d2650d
Osman M <osmanmunur@yahoo.com>
2024-10-08 12:11:47 +0300

    Initial Commit

---
+++ SimpleCV/SimpleCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCVApp.swift
+//  SimpleCV
+//
+//  Created by asia on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
